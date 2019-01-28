b23186f77aec890dfcabb9348e78ce6eef9686df
Andrea Picco <andreapicco@Andrea-3.local>
2019-01-28 14:54:06 +0100

    rename measure_spot_intensities as spotquant. add hearder with copyright and version

--- quantification.py
+++ quantification.py
@@ -1,4 +1,4 @@
-import bin.measure_spot_intensities as msi
+import bin.spotquant as sq 
 import numpy as np
 
-msi.experiment( path  =  '/Users/andreapicco/Desktop/For_Andrea/' , target_name = 'Mdm34' , reference_name = 'Cse4' , target_median_radius  =  6 , reference_median_radius  =  17 , only_membrane  =  False )
+sq.experiment( path  =  '/Users/andreapicco/Desktop/For_Andrea/' , target_name = 'Mdm34' , reference_name = 'Cse4' , target_median_radius  =  6 , reference_median_radius  =  17 , only_membrane  =  False )
