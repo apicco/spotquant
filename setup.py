13fa74252b73e1563f0b782b2a8208ec1294200a
Andrea Picco <andreapicco@Andrea-3.local>
2019-01-28 14:54:45 +0100

    the setup file for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from bin.spotquant import header
+
+setup( name = 'spot quantification' ,
+		version = header( printit = False ) ,
+		description = 'Utilities to quantify spot intensities in images' ,
+		author = 'Andrea Picco',
+		author_email = 'andrea.picco@unige.ch',
+		url = 'http://apicco.github.io/spotquant/',
+		download_url = 'https://github.com/apicco/spotquant/archive/master.zip',
+		packages = [ 'spotquant' ],
+		license = 'The software is distributed under the terms of the GNU General Public License Version 3, June 2007. Trajalign is a free software and comes with ABSOLUTELY NO WARRANTY.'
+		)
