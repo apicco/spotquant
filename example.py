bafec11c00c5aacb2c801831776a1b1809ef741d
Andrea Picco <andreapicco@Andrea-3.local>
2019-02-01 15:47:57 +0100

    a nicer example.

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ print("The number of target proteins is ")
 print( number_of_target_proteins )
 
 
-target , reference = sq.experiment( target_name = "Target_Protein" , path = "example/" , reference_name = 'Reference_Protein' , target_median_radius = 17 , reference_median_radius = 17 , measure_max_intensity_frame = False )
+target , reference = sq.experiment( target_name = "Target_Protein" , path = "example/" , reference_name = 'Reference_Protein' , target_median_radius = 17 , reference_median_radius = 17 , measure_max_intensity_frame = False , save_masks = False )
 number_of_target_proteins = sq.quantify( target , reference , r_number = ( 80 , 0 ) )
 print("The number of target proteins is ")
 print( number_of_target_proteins )
