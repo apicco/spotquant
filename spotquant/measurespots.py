be4de7db3969968da4dd55aa36e745fe90ff0184
Andrea Picco <andreapicco@Andrea-3.local>
2019-02-18 12:44:26 +0100

    remove test to check matplotlib versions

--- spotquant/measurespots.py
+++ spotquant/measurespots.py
@@ -6,7 +6,6 @@ from os import mkdir
 from skimage.measure import label
 import skimage.external.tifffile as tiff
 from matplotlib import pyplot as plt
-
 def header( version = 1.03 , year = 2019 , printit = True ) :
 
 	if printit :
